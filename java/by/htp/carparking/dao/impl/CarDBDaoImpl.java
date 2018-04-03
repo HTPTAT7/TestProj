cd39f1a256a526911ea79c18a7cf25b44bd1969a
HTPTAT7 <36242179+HTPTAT7@users.noreply.github.com>
2018-04-03 21:46:11 +0300

    Add files via upload

---
+++ java/by/htp/carparking/dao/impl/CarDBDaoImpl.java
@@ -0,0 +1,36 @@
+package by.htp.carparking.dao.impl;
+
+import java.util.List;
+
+import by.htp.carparking.dao.BaseDao;
+import by.htp.carparking.domain.Car;
+
+public class CarDBDaoImpl implements BaseDao<Car>{
+
+	@Override
+	public void create(Car entity) {
+		
+	}
+
+	@Override
+	public Car read(int id) {
+		return null;
+	}
+
+	@Override
+	public List<Car> readAll() {
+	
+		return null;
+	}
+
+	@Override
+	public void update(Car entity) {
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		
+	}
+
+}
